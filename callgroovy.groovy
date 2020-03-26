1768019d1e46a8fbc70df12cc5cdfdb737bf17a5
Chaitanya Senapati <csenapati12@gmail.com>
2020-03-26 22:03:56 +0530

    Update callgroovy.groovy

--- callgroovy.groovy
+++ callgroovy.groovy
@@ -1,24 +1,16 @@
- node(){
+node(){
     stage("Compare"){
               def completeJobName = "$JOB_NAME"
-               def actualJobName = "prod"
-               int len = actualJobName.length();
-               boolean occurance = false
-               int i = 0;
-               while (!completeJobName.regionMatches(i, actualJobName, 0, len)) {
-                       i++;
-                       occurance = true;
-               }
-               if (occurance) {
-                      print "Its a prod job"
-                       print("/var/lib/jenkins/prodworkspace")
-               }else
-               {
+              def actualJobName = "prod"
+              if(completeJobName.contains("Prod")){
+                  print "Its a prod job"
+                  echo "job name  ===   "+completeJobName
+                   print("/var/lib/jenkins/prodworkspace")
+              }else{
                    print("Its QA job")
+                  echo "job name  === "+completeJobName
                    print("/var/lib/jenkins/qaworkspace")
-               }
-                   
+              }               
                    
     }
 }
- 
